8730711258e38fb239efac9ed83c1c1669c7d3b6
Tony <tshen815@gmail.com>
2020-12-10 19:06:26 -0800

    cleaned up app, deleted unused tags and code from boilerplate and unused png files

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,30 +1,32 @@
 import React, {useState} from 'react'
 
 function TaskForm(props) {
-    const [input, setInput] = useState('')
+  const [input, setInput] = useState('')
 
-    const handleChange = e => {
-        setInput(e.target.value)
-    };
+  const handleChange = e => {
+		// console.log(e.target)
+      setInput(e.target.value)
+  };
 
-    const handleSubmit = e => {
-        // prevent screen reloading when todo button is clicked with new task 
-        e.preventDefault();
+  const handleSubmit = e => {
+      // prevent screen reloading when todo button is clicked with new task 
+      e.preventDefault();
+			// console.log('this is props for taskForm:',props)
+			// console.log('this is the event:', e)
+      props.onSubmit({
+          id: Math.floor(Math.random() * 10000),
+          text: input
+      });
 
-        // props.onSubmit({
-        //     id: Math.floor(Math.random() * 10000),
-        //     text: input
-        // });
-
-        setInput('');
-    }
-
-    return (
-        <form className="task-form" onSubmit={handleSubmit}>
-            <input type="text" placeholder="Add a task" value={input} name="text" className="task-input" onChange={handleChange}/>
-            <button className="task-button">Add Task</button>
-        </form>
-    )
+      setInput('');
+	}
+	
+	return (
+    <form className="task-form" onSubmit={handleSubmit}>
+      <input type="text" placeholder="Add a task" value={input} name="text" className="task-input" onChange={handleChange}/>
+      <button className="task-button">Add Task</button>
+    </form>
+  )
 }
 
 export default TaskForm
