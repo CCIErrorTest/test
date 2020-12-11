228d03952e64f3e299c18fdef922aeda7054f7ac
Tony <tshen815@gmail.com>
2020-12-10 19:04:05 -0800

    added further state modifiers for task list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,19 +2,24 @@ import React, {useState} from 'react';
 import TaskForm from './TaskForm.js';
 
 function TaskList() {
-const [task, setTask] = useState([])
+const [tasks, setTasks] = useState([])
 
 const addTask = task => {
+	// console.log('this is task', task)
 	// code to prevent multiple spaces in task input box, ensures that a task is inputed not just a space
 	if (!task.text || /^\s*$/.test(task.text)) {
 		return
 	}
-	
+
+	const newTasks = [task, ...tasks]
+
+	setTasks(newTasks)
+	// console.log('this is tasks:',...tasks)
 }
   return (
     <div>
       <h1>What Do You Have to Get Done Today?</h1>
-      <TaskForm/>
+      <TaskForm onSubmit={addTask}/>
     </div>
   )
 }
