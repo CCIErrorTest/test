2f491fdad12f41f8ae9c2b06da6fabd74fd8e597
Tony <tshen815@gmail.com>
2020-12-10 20:02:21 -0800

    added react icons to render with task div

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import TaskForm from './TaskForm';
 import TaskList from './TaskList';
+import {RiCloseCircleLine} from 'react-icons/ri';
+import {TiEdit} from 'react-icons/ti';
 
 
-function Task() {
+function Task({tasks, completeTask}) {
   const [edit, setEdit] = useState({
      id: null,
      value: ''
@@ -11,9 +13,17 @@ function Task() {
 
   return tasks.map((task, index) => {
 		<div className={task.isComplete ? 'task-row complete' : 'task-row'} key={index}>
-			
+
+			<div key={task.id} onClick={() => completeTask(task.id)}>
+				{task.text}
+			</div>
+			<div className='icons'>
+				<RiCloseCircleLine/>
+				<TiEdit/>
+			</div>
+
 		</div>
-	})
+	});
 }
 
 export default Task
