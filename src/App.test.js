8f7ac878656f7c1323c9a7bb4459c7218001a41c
Tony Shen <tshen815@gmail.com>
2021-04-07 13:42:07 -0700

    add test to check for task list header

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import {render, screen } from '@testing-library/react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
@@ -12,4 +13,10 @@ test('renders add task button', () => {
     render(<TaskForm/>)
     const btnElement = screen.getByText(/add task/i);
     expect(btnElement).toBeInTheDocument();
+})
+
+test("renders task list header to page", () => {
+    render(<TaskList/>)
+    const header = screen.getByText(/what do you have to get done today?/i);
+    expect(header).toBeInTheDocument();
 })
