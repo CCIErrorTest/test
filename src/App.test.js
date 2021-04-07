bc1e059248e43ddc3f0db5ec2789f8741bf12cea
Tony Shen <tshen815@gmail.com>
2021-04-07 12:38:12 -0700

    add test to check if add task button is on task form

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import {render, screen } from '@testing-library/react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TaskForm from "./components/TaskForm";
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App/>, div);
+})
+
+test('renders add task button', () => {
+    render(<TaskForm/>)
+    const btnElement = screen.getByText(/add task/i);
+    expect(btnElement).toBeInTheDocument();
 })
