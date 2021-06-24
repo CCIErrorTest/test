f3578ec8fb06b619d8af563ef213fda060b21a40
Tony <tshen815@gmail.com>
2021-06-24 16:06:24 -0700

    testing [ci skip]

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 function TaskForm(props) {
-  const [input, setInput] = useState(props.edit ? props.edit.value : '');
+	const [input, setInput] = useState(props.edit ? props.edit.value : '');
 
 	const inputRef = useRef(null);
 
@@ -9,57 +9,53 @@ function TaskForm(props) {
 		inputRef.current.focus();
 	});
 
-  const handleChange = e => {
-		// console.log(e.target)
-      setInput(e.target.value)
-  };
+	const handleChange = e => {
+		setInput(e.target.value)
+	};
 
-  const handleSubmit = e => {
-      // prevent screen reloading when todo button is clicked with new task 
-      e.preventDefault();
-			// console.log('this is props for taskForm:',props)
-			// console.log('this is the event:', e)
-      props.onSubmit({
-          id: Math.floor(Math.random() * 10000),
-          text: input
-      });
+	const handleSubmit = e => {
+		// prevent screen reloading when todo button is clicked with new task 
+		e.preventDefault();
+		props.onSubmit({
+			id: Math.floor(Math.random() * 10000),
+			text: input
+		});
 
-      setInput('');
+		setInput('');
 	}
-	
+
 	return (
-    <form className="task-form" onSubmit={handleSubmit}>
-			{props.edit ? (		
-			<>
-			<input 
-			type="text" 
-			placeholder="Edit your task" 
-			value={input} name="text" 
-			className="task-input edit" 
-			onChange={handleChange}
-			ref={inputRef}
-			/>
-			<button className="task-button edit">Edit Task</button>
-			</>
-			) : 
-			(		
-			<>
-				<input 
-				type="text" 
-				placeholder="Add a task" 
-				value={input} name="text" 
-				className="task-input" 
-				onChange={handleChange}
-				ref={inputRef}
-				/>
-				<button className="task-button">Add Task</button>
-			</>
-			)
+		<form className="task-form" onSubmit={handleSubmit}>
+			{props.edit ? (
+				<>
+					<input
+						type="text"
+						placeholder="Edit your task"
+						value={input} name="text"
+						className="task-input edit"
+						onChange={handleChange}
+						ref={inputRef}
+					/>
+					<button className="task-button edit">Edit Task</button>
+				</>
+			) :
+				(
+					<>
+						<input
+							type="text"
+							placeholder="Add a task"
+							value={input} name="text"
+							className="task-input"
+							onChange={handleChange}
+							ref={inputRef}
+						/>
+						<button className="task-button">Add Task</button>
+					</>
+				)
 			}
-	
-    </form>
-  )
+
+		</form>
+	)
 }
 
 export default TaskForm
- 
