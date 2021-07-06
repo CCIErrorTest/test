e9b5b9c11d029daca4829a8df6ae8d51a21541a9
Tony <tshen815@gmail.com>
2021-07-06 14:26:44 -0700

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import {render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import TaskForm from "./components/TaskForm";
@@ -6,30 +6,24 @@ import TaskList from "./components/TaskList";
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
-})
-
-test('renders add task button', () => {
-    render(<TaskForm/>)
-    const btnElement = screen.getByText(/add task/i);
-    expect(btnElement).toBeInTheDocument();
+    ReactDOM.render(<App />, div);
 })
 
 test("renders task list header to page", () => {
-    render(<TaskList/>)
+    render(<TaskList />)
     const header = screen.getByText(/what do you have to get done today?/i);
     expect(header).toBeInTheDocument();
 })
 
 test("renders task list header to page", () => {
-    render(<TaskList/>)
+    render(<TaskList />)
     const header = screen.getByText(/what do you have to get done today?/i);
     expect(header).toBeInTheDocument();
 })
 
 
 test("renders task list subheader to page", () => {
-    render(<TaskList/>)
+    render(<TaskList />)
     const subheader = screen.getByText(/Dont procrastinate!/i);
     expect(subheader).toBeInTheDocument();
 })
