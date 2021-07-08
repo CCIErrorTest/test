e2101833f665104c09220065158c49bc0cdb1ff6
Tony <tshen815@gmail.com>
2021-07-08 15:37:58 -0700

    test

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,54 +1,54 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import TaskForm from './TaskForm.js';
 import Task from './Task';
 
 function TaskList() {
-const [tasks, setTasks] = useState([])
+	const [tasks, setTasks] = useState([])
 
-const addTask = task => {
-	// console.log('this is task', task)
-	// code to prevent multiple spaces in task input box, ensures that a task is inputed not just a space
-	if (!task.text || /^\s*$/.test(task.text)) {
-		return;
-	}
-
-	const newTasks = [task, ...tasks]
-
-	setTasks(newTasks)
-	// console.log('this is tasks:',...tasks)
-}
-
-const updateTask = (taskId, newValue) => {
-	if (!newValue.text || /^\s*$/.test(newValue.text)) {
-		return;
-	}
-	setTasks(prev => prev.map(item => (item.id === taskId ? newValue : item)));
-};
-
-const removeTask = id => {
-	const removeArr = [...tasks].filter(task => task.id !== id);
-	setTasks(removeArr);
-	return;
-};
-
-const completeTask = id => {
-	let updatedTasks = tasks.map(task => {
-		if (task.id === id) {
-			task.isComplete = !task.isComplete;
+	const addTask = task => {
+		// console.log('this is task', task)
+		// code to prevent multiple spaces in task input box, ensures that a task is inputed not just a space
+		if (!task.text || /^\s*$/.test(task.text)) {
+			return;
 		}
-		return task;
-	});
-	setTasks(updatedTasks);			
-};
 
-  return (
-    <div>
-      <h1>What Do You Have to Get Done Today?</h1>
-	  <h2>Dont procrastinate!</h2>
-      <TaskForm onSubmit={addTask}/>
-			<Task tasks={tasks} completeTask={completeTask} removeTask={removeTask} updateTask={updateTask}/>
-    </div>
-  );
+		const newTasks = [task, ...tasks]
+
+		setTasks(newTasks)
+		// console.log('this is tasks:',...tasks)
+	}
+
+	const updateTask = (taskId, newValue) => {
+		if (!newValue.text || /^\s*$/.test(newValue.text)) {
+			return;
+		}
+		setTasks(prev => prev.map(item => (item.id === taskId ? newValue : item)));
+	};
+
+	const removeTask = id => {
+		const removeArr = [...tasks].filter(task => task.id !== id);
+		setTasks(removeArr);
+		return;
+	};
+
+	const completeTask = id => {
+		let updatedTasks = tasks.map(task => {
+			if (task.id === id) {
+				task.isComplete = !task.isComplete;
+			}
+			return task;
+		});
+		setTasks(updatedTasks);
+	};
+
+	return (
+		<div>
+			<h1>What Do You Have to Get Done Today?</h1>
+			<h2>Dont pcrastinate!</h2>
+			<TaskForm onSubmit={addTask} />
+			<Task tasks={tasks} completeTask={completeTask} removeTask={removeTask} updateTask={updateTask} />
+		</div>
+	);
 };
 
 export default TaskList
